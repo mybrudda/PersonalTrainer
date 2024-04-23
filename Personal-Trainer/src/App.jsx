58b303deec45e7604df30ede975a741129175260
mybrudda <149365096+mybrudda@users.noreply.github.com>
2024-04-23 14:31:37 +0300

    Navbar implemented with React router

--- Personal-Trainer/src/App.jsx
+++ Personal-Trainer/src/App.jsx
@@ -1,8 +1,37 @@
 import { AppBar, Button, Toolbar, Typography } from '@mui/material';
 import { useState } from 'react';
+import { Link, Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import './App.css';
 import Customer from './components/Customer';
 import Training from './components/Training';
+
+function NavigationBar({ handleTabChange }) {
+  return (
+    <AppBar position="fixed" style={{ height: '70px' }}> 
+      <Toolbar style={{ justifyContent: 'center' }}> 
+        <Typography variant="h4" component="div">
+          Fitness Trainer
+        </Typography>
+      </Toolbar>
+      <Toolbar style={{ justifyContent: 'center' }}>
+        <Button
+          color="inherit"
+          component={Link}
+          to="/training"
+        >
+          Training
+        </Button>
+        <Button
+          color="inherit"
+          component={Link}
+          to="/customer"
+        >
+          Customer
+        </Button>
+      </Toolbar>
+    </AppBar>
+  );
+}
 
 function App() {
   const [selectedTab, setSelectedTab] = useState('training');
@@ -12,36 +41,18 @@ function App() {
   };
 
   return (
-    <>
-      <AppBar position="fixed" style={{ height: '70px' }}> 
-        <Toolbar style={{ justifyContent: 'center' }}> 
-          <Typography variant="h4" component="div">
-            Fitness Trainer
-          </Typography>
-        </Toolbar>
-        <Toolbar style={{ justifyContent: 'center' }}>
-          <Button
-            color={selectedTab === 'training' ? 'primary' : 'inherit'}
-            onClick={() => handleTabChange('training')}
-          >
-            Training
-          </Button>
-          <Button
-            color={selectedTab === 'customer' ? 'primary' : 'inherit'}
-            onClick={() => handleTabChange('customer')}
-          >
-            Customer
-          </Button>
-        </Toolbar>
-      </AppBar>
-
-      <Toolbar />
-
-      <div style={{ marginBottom: '30%' }}> 
-        {selectedTab === 'training' ? <Training /> : <Customer />}
-      </div>
-    </>
-  )
+    <div>
+      <Router>
+        <NavigationBar />
+        <Toolbar />
+        <Routes>
+          <Route path="/training" element={<Training />} />
+          <Route path="/customer" element={<Customer />} />
+          <Route path="*" element={<h1>Page not found!</h1>} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
